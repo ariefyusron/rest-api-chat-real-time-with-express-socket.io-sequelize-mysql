147b4d1eb36e609076684f07b31d9e0b41ecdcc8
Arief Yusron <ariefyusron98@gmail.com>
2019-01-12 00:44:46 +0700

    build sendChat

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -15,12 +15,28 @@ exports.index = (req,res) => {
 
 }
 
-//showChat
-exports.show = (req,res) => {
-  res.json('sendChat')
-}
-
 //sendChat
 exports.send = (req,res) => {
+
+  req.check('chat','Chat is required').not().isEmpty()
+
+  const errors = req.validationErrors()
+  if (errors){
+    res.status(400).json(errors)
+  } else{
+    models.Chat.create({
+      chat: req.body.chat,
+      fromUserId:req.userData.id,
+      toUserId:req.params.id
+    })
+      .then((results) => {
+        res.json(results)
+      })
+  }
+
+}
+
+//showChat
+exports.show = (req,res) => {
   res.json('sendChat')
 }
