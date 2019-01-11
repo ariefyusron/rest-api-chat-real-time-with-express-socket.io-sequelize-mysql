1a6c2b14df464d2b6237b4708ff3def31ddcedb0
Arief Yusron <ariefyusron98@gmail.com>
2019-01-11 22:46:54 +0700

    build auth

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,7 +1,78 @@
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
+const models = require('../models')
+const secret_key = process.env.JWT_SECRET
+
+//register
 exports.register = (req,res) => {
-  res.json('register')
+
+  req.check('firstName','Firstname is required').not().isEmpty()
+  req.check('lastName','Lastname is required').not().isEmpty()
+  req.check('username','username is required').not().isEmpty()
+  req.check('email','email is not valid').isEmail()
+  req.check('password','Password min 8').isLength({min:8}).equals(req.body.confirmPassword).withMessage('Confirm password is different')
+
+  const errors = req.validationErrors()
+  if (errors){
+    res.status(400).json(errors)
+  } else {
+    req.body.password = bcrypt.hashSync(req.body.password, saltRounds)
+    models.User.create(req.body)
+      .then((results) => {
+        res.json(results)
+      }).catch((next) => {
+        res.status(400).json({
+          message: next.fields.email? 'email is already':'username is already'
+        })
+      })
+  }
+
 }
 
-exports.login = (res,req) => {
-  res.json('login')
+//login
+exports.login = (req,res) => {
+  
+  req.check('username').isEmail()
+
+  const errors = req.validationErrors()
+  if (errors){
+    models.User.findOne({
+      where:{
+        username:req.body.username
+      }
+    }).then((result) => {
+      const compare = bcrypt.compareSync(req.body.password, result.password)
+      if (compare){
+        const token = jwt.sign({id:result.id,username:result.username,email:result.email},secret_key)
+        res.json({token:token})
+      } else {
+        res.status(400).json({message: 'Password not valid'})
+      }
+    }).catch(() => {
+      res.status(400).json({
+        message: 'Username not yet registered'
+      })
+    })
+  } else {
+    models.User.findOne({
+      where:{
+        email:req.body.username
+      }
+    }).then((result) => {
+      const compare = bcrypt.compareSync(req.body.password, result.password)
+      if (compare){
+        const token = jwt.sign({id:result.id,username:result.username,email:result.email},secret_key)
+        res.json({token:token})
+      } else{
+        res.status(400).json({message: 'Password not valid'})
+      }
+    }).catch(() => {
+      res.status(400).json({
+        message: 'Email not yet registered'
+      })
+    })
+  }
+
 }
--- app/migrations/20190110190112-create-user.js
+++ app/migrations/20190110190112-create-user.js
@@ -15,10 +15,12 @@ module.exports = {
         type: Sequelize.STRING
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
